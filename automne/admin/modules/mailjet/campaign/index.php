5fe2014cb9d50f5fc42492e5626fabf080861109
Arthur Gorjux <arthur.gorjux@ws-interactive.fr>
2015-11-09 15:49:25 +0100

    Fix bug index campaigns

--- automne/admin/modules/mailjet/campaign/index.php
+++ automne/admin/modules/mailjet/campaign/index.php
@@ -66,50 +66,52 @@ foreach ($response->Data as $campaign) {
 
 					</div>
 				<?php if($api->_response_code === MailjetAPI::MAILJET_STATUS_CODE_OK_VIEW): ?>
-				<div class="widget-box">
-				<div class="widget-content nopadding table-responsive">
-					<table class="table table-bordered table-striped table-hover">
-						<thead>
-							<tr>
-								<th>Statut</th>
-								<th>Titre</th>
-								<th>Sujet</th>
-								<th>Date de création</th>
-								<th>Date d'envoi</th>
-								<th>Actions</th>
-							</tr>
-						</thead>
-						<tbody>
-						<?php foreach ($campaigns as $campaign):?>
-							<tr>
-								<td><span class="label label-<?php echo MailjetAPI::getStatusClass($campaign['status'])?>"><?php echo MailjetAPI::getStatus($campaign['status'])?></span></td>
-								<td><?php echo $campaign['title']?></td>
-								<td><?php echo $campaign['subject']?></td>
-								<td><?php echo (isset($campaign['createdAt']) && $campaign['createdAt'] != 0) ? date_create($campaign['createdAt'])->format('d/m/Y') : '-'?></td>
-								<td><?php echo (isset($campaign['sendAt']) && $campaign['sendAt'] != 0) ? date_create($campaign['sendAt'])->format('d/m/Y H:i') : '-' ?></td>
-								<td>
-									<a href="view.php?id=<?php echo $campaign['id'];?>" class="btn btn-primary btn-xs"><i class="icon icon-edit"></i> Modifier</a>
-									<?php if ($campaign['status'] !== 'archived' && $campaign['status'] !== 'sent' && $campaign['status'] !== 'programmed'):?>
-										<a href="write.php?id=<?php echo $campaign['id'];?>"class="btn btn-success btn-xs"><i class="icon-remove icon-pencil"></i> Rédiger</a>
-									<?php endif; ?>
-									<?php if (isset($campaign['url']) && $campaign['url'] !== ''): ?>
-										<a href="<?php echo $campaign['url'];?>" target="_blank" class="btn btn-primary btn-xs"><i class="icon icon-eye-open"></i> Voir en ligne</a>
-									<?php endif ?>
-									<?php if (isset($campaign->report_uri)): ?>
-										<a href="https://fr.mailjet.com<?php echo $campaign->report_uri;?>" target="_blank" class="btn btn-primary btn-xs"><i class="icon icon-signal"></i> Consulter le rapport</a>
-									<?php endif ?>
-								</td>
-							</tr>
-						<?php endforeach ?>
-						</tbody>
-					</table>
-				</div>
-			</div>
-				<?php else:?>
-					<div class="alert alert-danger">
-						<button class="close" data-dismiss="alert">×</button>
-						<strong>Erreur!</strong> Récupération des campagnes impossible.
+					<?php if(!empty($campaigns)) : ?>
+					<div class="widget-box">
+					<div class="widget-content nopadding table-responsive">
+						<table class="table table-bordered table-striped table-hover">
+							<thead>
+								<tr>
+									<th>Statut</th>
+									<th>Titre</th>
+									<th>Sujet</th>
+									<th>Date de création</th>
+									<th>Date d'envoi</th>
+									<th>Actions</th>
+								</tr>
+							</thead>
+							<tbody>
+							<?php foreach ($campaigns as $campaign):?>
+								<tr>
+									<td><span class="label label-<?php echo MailjetAPI::getStatusClass($campaign['status'])?>"><?php echo MailjetAPI::getStatus($campaign['status'])?></span></td>
+									<td><?php echo $campaign['title']?></td>
+									<td><?php echo $campaign['subject']?></td>
+									<td><?php echo (isset($campaign['createdAt']) && $campaign['createdAt'] != 0) ? date_create($campaign['createdAt'])->format('d/m/Y') : '-'?></td>
+									<td><?php echo (isset($campaign['sendAt']) && $campaign['sendAt'] != 0) ? date_create($campaign['sendAt'])->format('d/m/Y H:i') : '-' ?></td>
+									<td>
+										<a href="view.php?id=<?php echo $campaign['id'];?>" class="btn btn-primary btn-xs"><i class="icon icon-edit"></i> Modifier</a>
+										<?php if ($campaign['status'] !== 'archived' && $campaign['status'] !== 'sent' && $campaign['status'] !== 'programmed'):?>
+											<a href="write.php?id=<?php echo $campaign['id'];?>"class="btn btn-success btn-xs"><i class="icon-remove icon-pencil"></i> Rédiger</a>
+										<?php endif; ?>
+										<?php if (isset($campaign['url']) && $campaign['url'] !== ''): ?>
+											<a href="<?php echo $campaign['url'];?>" target="_blank" class="btn btn-primary btn-xs"><i class="icon icon-eye-open"></i> Voir en ligne</a>
+										<?php endif ?>
+										<?php if (isset($campaign->report_uri)): ?>
+											<a href="https://fr.mailjet.com<?php echo $campaign->report_uri;?>" target="_blank" class="btn btn-primary btn-xs"><i class="icon icon-signal"></i> Consulter le rapport</a>
+										<?php endif ?>
+									</td>
+								</tr>
+							<?php endforeach ?>
+							</tbody>
+						</table>
 					</div>
+				</div>
+					<?php else:?>
+						<div class="alert alert-danger">
+							<button class="close" data-dismiss="alert">×</button>
+							<strong>Erreur!</strong> Récupération des campagnes impossible.
+						</div>
+					<?php endif; ?>
 				<?php endif;?>
 			</div>
 			</div>
